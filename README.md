afd9be0d366c0cd953419fa234ae3cee3fdad9d1
Pete Garcia <petegarcia54@sbcglobal.net>
2018-02-21 16:20:37 -0600

    working version

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# burger
+# Eat-Da-Burger - Node, Express, Handlebars, ORM
+
+### Overview
+Order your burger, and eat it too!
+
+ 
---
+++ config/connection.js
@@ -0,0 +1,21 @@
+
+var mysql = require("mysql");
+
+
+var connection;
+var PORT = process.env.PORT || 3000;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "burgers_db"
+  });
+};
+
+
+connection.connect();
+module.exports = connection;
